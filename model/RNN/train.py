a547da6026e7a4fe2590b944056a7789382e3804
hallogameboy <hallogameboy@gmail.com>
2017-06-12 19:08:39 -0700

    Update the current RNN hyperparameter

--- model/RNN/train.py
+++ model/RNN/train.py
@@ -24,7 +24,7 @@ tf.flags.DEFINE_float("l2_reg_lambda", 0.01, "L2 regularization lambda (default:
 
 # Training parameters
 tf.flags.DEFINE_integer("batch_size", 64, "Batch Size (default: 64)")
-tf.flags.DEFINE_integer("num_epochs", 100, "Number of training epochs (default: 200)")
+tf.flags.DEFINE_integer("num_epochs", 1000, "Number of training epochs (default: 200)")
 tf.flags.DEFINE_integer("evaluate_every", 200, "Evaluate model on dev set after this many steps (default: 100)")
 tf.flags.DEFINE_integer("checkpoint_every", 400, "Save model after this many steps (default: 100)")
 tf.flags.DEFINE_integer("num_checkpoints", 50, "Number of checkpoints to store (default: 5)")
